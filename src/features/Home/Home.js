4a642ac9afe3e1e9272e67cd793fe87d2615c7a2
IrynaRybka <irarybka@gmail.com>
2023-02-14 11:29:53 +0200

    home animation

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -1,9 +1,11 @@
 import css from './Home.module.css';
+import {MdContactPhone} from 'react-icons/md';
 export const Home =() => {
     return (
         <div className={css.home}>
             <h1>Save your contacts here</h1>
             <p>Welcome page 🤗</p>
+           <MdContactPhone className={css.picture} size={200}/>
         </div>
     )
 }
