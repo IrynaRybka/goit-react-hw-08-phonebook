fd1d4b5a1272a4192271e7a90da1bf3e9e2c0837
IrynaRybka <irarybka@gmail.com>
2023-11-29 21:07:37 +0200

    change onAddContact

--- src/features/Contacts/ContactsForm/ContactsForm.js
+++ src/features/Contacts/ContactsForm/ContactsForm.js
@@ -28,7 +28,7 @@ export const ContactsForm = () => {
       return false;
     }
     dispatch(addContactsAsyncThunk({ name, number }));
-    reset();
+    // reset();
   };
 
   const reset = () => {
@@ -55,8 +55,7 @@ export const ContactsForm = () => {
             onChange={e => setName(e.target.value)}
             type="text"
             name="name"
-            // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            pattern="\w{3,20}"
             required
           />
         </label>
@@ -71,7 +70,7 @@ export const ContactsForm = () => {
             onChange={e => setNumber(e.target.value)}
             type="tel"
             name="number"
-            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
