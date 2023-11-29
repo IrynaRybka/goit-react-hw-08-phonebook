9bcf779bde6920e2de0627694cd81fc979c1d2be
IrynaRybka <irarybka@gmail.com>
2023-11-29 21:15:08 +0200

    return version

--- src/features/Contacts/ContactsForm/ContactsForm.js
+++ src/features/Contacts/ContactsForm/ContactsForm.js
@@ -70,7 +70,7 @@ export const ContactsForm = () => {
             onChange={e => setNumber(e.target.value)}
             type="tel"
             name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
           />
